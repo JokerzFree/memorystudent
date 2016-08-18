4d9b87f58fb8ddeb8897b518209535a4578c850e
JokerzFree <urbanovichwork@gmail.com>
2016-08-18 13:07:16 +0300

    we

--- www/js/script.js
+++ www/js/script.js
@@ -11,7 +11,9 @@ $( document ).on( "pagecreate", "#page", function() {
             if ( e.type === "swiperight" ) {
                 $( "#left-panel" ).panel( "open" );
             }
-        }
+        } else if ($( ".ui-page-active" ).jqmData( "panel" ) == "open"){
+			$( "#left-panel" ).panel( "close" );
+    	}
     });
 
     $(document).on("swipeleft", "#page", function( e ){
@@ -29,7 +31,23 @@ $( document ).on( "pagecreate", "#page", function() {
             } else {
                 $("#main").html(info[href]);
             }
+            //
+            switch (href){
+                case 'administracia','raspisanie':
+                $("#table").table("refresh");
+                break;
+            }
+            //
             $("[data-role=panel]").panel("close");
         }
+    });
+
+    $(document).on('tap', 'a[href^=http], a[href^=https]', function(e){
+
+        e.preventDefault();
+        var $this = $(this); 
+        var target = $this.data('inAppBrowser') || '_system';
+
+        window.open($this.attr('href'), target, 'location=no');
     });
 });
